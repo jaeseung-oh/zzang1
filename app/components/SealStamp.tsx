1905d729071ff343dd61dc06110fc98fc892b600
cfv470 <cfv470@gmail.com>
2026-07-02 12:15:31 +0000

    Use latest uploaded logos

--- app/components/SealStamp.tsx
+++ app/components/SealStamp.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 export const sealStampPath = "/images/%EB%A6%AC%EC%85%8B%EC%97%90%EB%93%80%EC%84%BC%ED%84%B0%20%EC%A7%81%EC%9D%B8.png";
-export const centerLogoPath = "/images/resetedu-logo-mark.png";
-export const centerFullLogoPath = "/images/resetedu-center-logo.png";
+export const centerLogoPath = "/images/resetedu-document-watermark.png";
+export const centerFullLogoPath = "/images/resetedu-header-logo.jpg";
 
 export type SealStampProps = {
   size?: number;
